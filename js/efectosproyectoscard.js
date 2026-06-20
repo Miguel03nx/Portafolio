587ff117cd710625647a38c23ad6d62b125e626b
Miguel03nx <161259499+Miguel03nx@users.noreply.github.com>
2026-06-19 18:02:24 -0600

    cambios
    mejoras a la parte de proyectos

--- js/efectosproyectoscard.js
+++ js/efectosproyectoscard.js
@@ -40,7 +40,7 @@ const paginas = [
             },
 
             {
-                 nombre: "Landing festival",
+                nombre: "Landing festival",
                 descripcion: "HTML, CSS, SASS, JavaScript",
                 link: "https://festivalrockedmmexico.netlify.app"
                 // imagen: "media/blog-cafe.png",
@@ -57,28 +57,28 @@ function cargarPagina() {
 
     const pagina = paginas[paginaActual];
 
-    const nombre1 = document.querySelector("#nombre1");
-    const nombre2 = document.querySelector("#nombre2");
+    const imagen1 = document.querySelector("#img1");
+    // const nombre1 = document.querySelector("#nombre1");
 
-    nombre1.textContent = pagina[0].nombre;
-    nombre2.textContent = pagina[1].nombre;
+    imagen1.src = pagina[0].imagen;
+    // nombre1.textContent = pagina[0].nombre;
 
-    const descripcion1 = document.querySelector("#descripcion1");
+    // const descripcion1 = document.querySelector("#descripcion1");
     const descripcion2 = document.querySelector("#descripcion2");
 
-    descripcion1.textContent = pagina[0].descripcion;
+    // descripcion1.textContent = pagina[0].descripcion;
     descripcion2.textContent = pagina[1].descripcion;
 
-    const link1 = document.querySelector("#link1");
+    // const link1 = document.querySelector("#link1");
     const link2 = document.querySelector("#link2");
 
-    link1.href = pagina[0].link;
+    // link1.href = pagina[0].link;
     link2.href = pagina[1].link;
 
-    const img1 = document.querySelector("#img1");
-    const img2 = document.querySelector("#img2");
-    img1.src = pagina[0].imagen;
-    img2.src = pagina[1].imagen;
+    // const img1 = document.querySelector("#img1");
+    // const img2 = document.querySelector("#img2");
+    // img1.src = pagina[0].imagen;
+    // img2.src = pagina[1].imagen;
 
     // puntos del carruser para las paginas de los proyectos
     const dot1 = document.querySelector("#dot1");
